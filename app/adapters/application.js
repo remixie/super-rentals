99fe2ca7cf9ae9b3744aa1c807cff7ff20dc02c9
Francisco Salinas <remixielive@live.com>
2022-05-31 14:04:38 -0400

    Manual Linting of Files.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import JSONAPIAdapter from '@ember-data/adapter/json-api';
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
   //namespace = 'api';
-  host = 'http://localhost:3000'
+  host = 'http://localhost:3000';
 
   buildURL(...args) {
     return `${super.buildURL(...args)}`;
